29529b3b2ac9f5a533817039ce32ca9ab54ab6c2
Hasnpreet Sandhu <hs17.sandhu@surreyschools.ca>
2023-01-19 11:15:51 -0800

    Jan 19

---
+++ CorePathway/Assets/Scripts/Rotate.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    
+    void Update()
+    {
+        transform.Rotate(new Vector3(0f, 50f, 0f) * Time.deltaTime);
+    }
+}
