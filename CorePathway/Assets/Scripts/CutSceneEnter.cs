108cb3b1a3ec45d9f50e1c4e01d3e7f1e47e0d3f
moosewala21 <113132586+moosewala21@users.noreply.github.com>
2023-04-03 10:44:24 -0700

    april 3

--- CorePathway/Assets/Scripts/CutSceneEnter.cs
+++ CorePathway/Assets/Scripts/CutSceneEnter.cs
@@ -6,20 +6,33 @@ public class CutSceneEnter : MonoBehaviour
 {
     public GameObject thePlayer;
     public GameObject cutsceneCam;
+    public GameObject cutsceneCam2;
 
     void OnTriggerEnter(Collider other)
     {
         this.gameObject.GetComponent<BoxCollider>().enabled = false;
         cutsceneCam.SetActive(true);
+        cutsceneCam2.SetActive(false);
         thePlayer.SetActive(false);
+        StartCoroutine(KeepGoing());
+    }
+
+    IEnumerator KeepGoing()
+    {
+        yield return new WaitForSeconds(2);
+        thePlayer.SetActive(false);
+        cutsceneCam.SetActive(false);
+        cutsceneCam2.SetActive(true);
         StartCoroutine(FinishCut());
     }
 
     IEnumerator FinishCut()
     {
-        yield return new WaitForSeconds(10);
+        yield return new WaitForSeconds(9);
         thePlayer.SetActive(true);
         cutsceneCam.SetActive(false);
+        cutsceneCam2.SetActive(false);
+        
     }
 
 
